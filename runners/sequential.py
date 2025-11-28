90cb3c0f335547298e4fb085fca3f57e38a17bd1
Divvela Rakesh <147180905+divvelarakesh1@users.noreply.github.com>
2025-11-28 18:40:15 +0530

    Update sequential.py

--- runners/sequential.py
+++ runners/sequential.py
@@ -1,9 +1,6 @@
 import time
-# These imports work assuming you run the script from the project root
 from core.scanner import TLSScannerCore
 from core.models import ScannerConfig, ScanTarget
-
-# Import all checks
 from checks.certificates import DeepCertificateAnalysisCheck, DelegatedCredentialsCheck
 from checks.protocols import ProtocolSupportCheck
 from checks.ciphers import CipherConfigurationCheck, SessionTicketCheck
@@ -11,19 +8,19 @@ from checks.attacks import  ZombiePoodleCheck,TicketBleedCheck
 from checks.feature import AlpnCheck, TlsCompressionCheck, OcspStaplingCheck
 
 def run_scan(targets: list[ScanTarget]):
-    """
-    Sequential Runner: Scans one target at a time (Blocking I/O).
-    """
+    
+    #Sequential Runner: Scans one target at a time (Blocking I/O).
+    
     results = []
     
-    # Initialize config
+    
     config = ScannerConfig(connection_timeout=5.0, verify_certificates=False)
     
     for target in targets:
-        # Initialize scanner
+        
         scanner = TLSScannerCore(config)
         
-        # Register checks
+        
         scanner.register_check(DeepCertificateAnalysisCheck())
         scanner.register_check(DelegatedCredentialsCheck())
         scanner.register_check(ProtocolSupportCheck())
@@ -34,7 +31,7 @@ def run_scan(targets: list[ScanTarget]):
         scanner.register_check(TlsCompressionCheck())
         scanner.register_check(OcspStaplingCheck())
         
-        # Scan (Blocking)
+        
         result = scanner.scan(target)
         results.append(result)
 
